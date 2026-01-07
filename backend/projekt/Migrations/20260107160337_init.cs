28a8572781be2d13a902769e2c0bc37c41a13bf4
Olek Łukasiewicz <aleksander_lukasiewicz@outlook.com>
2026-01-07 17:15:09 +0100

    structure small change

---
+++ backend/projekt/Migrations/20260107160337_init.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace projekt.Migrations
+{
+    /// <inheritdoc />
+    public partial class init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DaysCount",
+                table: "LeaveRequests");
+
+            migrationBuilder.RenameColumn(
+                name: "Type",
+                table: "LeaveRequests",
+                newName: "LeaveType");
+
+            migrationBuilder.RenameColumn(
+                name: "Reason",
+                table: "LeaveRequests",
+                newName: "Comment");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "LeaveType",
+                table: "LeaveRequests",
+                newName: "Type");
+
+            migrationBuilder.RenameColumn(
+                name: "Comment",
+                table: "LeaveRequests",
+                newName: "Reason");
+
+            migrationBuilder.AddColumn<int>(
+                name: "DaysCount",
+                table: "LeaveRequests",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
